75a53d29b2f98717025060cad0a2efd0ec17955a
VINIT SINGH <130830294+Vinit311994@users.noreply.github.com>
2023-07-01 00:05:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function threeSum(arr, target) {
-// write your code here
-	  arr.sort((a, b) => a - b); 
+  arr.sort((a, b) => a - b); 
   let closestSum = Infinity;
 	for (let i = 0; i < arr.length - 2; i++) {
     let left = i + 1;
@@ -22,8 +21,4 @@ function threeSum(arr, target) {
 let target = 1;
 let ans = threeSum(arr, target);
 console.log(ans);
-
-  
-}
-
-module.exports = threeSum;
+module.exports = threeSum;
